19e71095b57785e7e9c2b177f15f864e51c0232c
jananicic <jananicic@yahoo.com>
2018-11-22 15:17:52 +0100

    Updates

---
+++ precache-manifest.80e535a49e58246288bd447d96faf760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "05a4f8f5cad93ef342cf",
+    "url": "/RoboFriends/static/js/main.05a4f8f5.chunk.js"
+  },
+  {
+    "revision": "40c0eba58a69a87ab868",
+    "url": "/RoboFriends/static/js/1.40c0eba5.chunk.js"
+  },
+  {
+    "revision": "05a4f8f5cad93ef342cf",
+    "url": "/RoboFriends/static/css/main.66bd9436.chunk.css"
+  },
+  {
+    "revision": "40c0eba58a69a87ab868",
+    "url": "/RoboFriends/static/css/1.034e7a15.chunk.css"
+  },
+  {
+    "revision": "993974bb27378b3302a8cc086954ddb4",
+    "url": "/RoboFriends/index.html"
+  }
+];
